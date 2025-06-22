8b927cb49dafb65fb8098c9872450a63a2bbaa05
Mudaseer <mudaseer2753@gmail.com>
2025-06-22 11:38:05 +0530

    updated extractor.py

--- backend/extractor.py
+++ backend/extractor.py
@@ -1,8 +1,12 @@
-from PyPDF2 import PdfReader
+# from PyPDF2 import PdfReader
 from pdf2image import convert_from_bytes
 from PIL import Image
 import pytesseract
 import io
+
+# Manual path setup (Poppler + Tesseract)
+POPPLER_PATH = r"C:\Users\mudas\Downloads\Release-24.08.0-0\poppler-24.08.0\Library\bin"
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 def extract_text_from_pdf(pdf_file):
     text = ""
@@ -14,7 +18,12 @@ def extract_text_from_pdf(pdf_file):
             if page_text:
                 text += page_text
             else:
-                images = convert_from_bytes(file_bytes, first_page=page_num, last_page=page_num)
+                images = convert_from_bytes(
+                    file_bytes,
+                    first_page=page_num,
+                    last_page=page_num,
+                    poppler_path=POPPLER_PATH
+                )
                 for img in images:
                     text += pytesseract.image_to_string(img)
     except Exception as e:
@@ -23,5 +32,5 @@ def extract_text_from_pdf(pdf_file):
     return text
 
 def ocr_from_image(file_bytes):
-    pages = convert_from_bytes(file_bytes)
+    pages = convert_from_bytes(file_bytes, poppler_path=POPPLER_PATH)
     return ''.join(pytesseract.image_to_string(img) for img in pages)
